a5537f8f5a7ce83d46e5f2c7b4fe21253058a9e3
erik gardea <erik.gardea@nerdery.com>
2021-01-15 14:43:46 -0600

    CRUD first commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input v-model="title" type="text" name="title" placeholder="Add Todo...">
+            <input type="submit" name="submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo',
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(event) {
+            event.preventDefault();
+
+            const newTodo = {
+                id: Math.floor(Math.random() * 100),
+                title: this.title,
+                completed: false
+            }
+
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template> 
+    <div class="todoItem" v-bind:class="{'todoItem--isCompleted':todo.completed}">
+        <p>
+            <input type="checkbox" :checked="todo.completed" @change="markComplete">
+            {{ todo.title }}
+            <button @click="$emit('del-todo', todo.id)" class="todoItem__deleteBtn">X</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        todo: {
+            type: Object,
+            default: () => {},
+        },
+    },
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todoItem {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+        color: #000000;
+    }
+    .todoItem--isCompleted {
+        text-decoration: line-through;
+    }
+    .todoItem__deleteBtn {
+        background: #ff0000;
+        color: #ffffff;
+        border: none;
+        border-radius: 50%;
+        height: 25px;
+        width: 25px;
+        cursor: pointer;
+    }
+</style>
